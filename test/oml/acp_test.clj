f9f7670a493c3336af17f1999bf4dc280c556024
Andy Smith <me@andysmith.ai>
2026-09-19 20:20:01 +0700

    test: drop diagnostic debug dump from ok connect test

--- test/oml/acp_test.clj
+++ test/oml/acp_test.clj
@@ -57,12 +57,7 @@
           alive?)))))
 
 (deftest connect-negotiates-v1-and-exposes-capabilities
-  (let [dbg (doto (File/createTempFile "acp-fake" ".log") .deleteOnExit)
-        conn (try
-               (connect-fake "ok" {"ACP_FAKE_DEBUG" (.getPath dbg)})
-               (catch Throwable t
-                 (println "ACP_FAKE_DEBUG:\n" (when (.isFile dbg) (slurp dbg)))
-                 (throw t)))
+  (let [conn (connect-fake "ok")
         caps (acp/capabilities conn)]
     (is (= 1 (:protocol-version caps)))
     (is (:load-session caps))
